346f54d52404821860ce2866bcf8d968cbdc45e9
shahaalap <68823276+shahaalap@users.noreply.github.com>
2026-04-17 19:42:32 -0400

    Add: count-number-of-islands - submission-2

---
+++ Data Structures & Algorithms/count-number-of-islands/submission-2.py
@@ -0,0 +1,28 @@
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        n, m = len(grid), len(grid[0])
+        neighbors = [[1,0],[-1,0],[0,1],[0,-1]]
+        q = deque()
+        result = 0
+
+        def bfs():
+            while len(q):
+                i, j = q.popleft()
+                
+                for ni,nj in neighbors:
+                    nei, nej = i + ni, j + nj
+                    if nei >= 0 and nei < n and nej >= 0 and nej < m and grid[nei][nej] == "1":
+                        q.append([nei,nej])
+                        grid[nei][nej] = "0"
+
+
+        for i in range(n):
+            for j in range(m):
+                if grid[i][j] == "1":
+                    grid[i][j] = "0"
+                    result += 1
+                    q.append([i,j])
+                    bfs()
+
+        return result
+            
