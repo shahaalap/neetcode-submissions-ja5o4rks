adaa0eeecbd17519e89e26716eaebbecd2df5abd
shahaalap <68823276+shahaalap@users.noreply.github.com>
2026-04-21 15:10:04 -0400

    Add: jump-game-ii - submission-9

---
+++ Data Structures & Algorithms/jump-game-ii/submission-9.py
@@ -0,0 +1,23 @@
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        q = deque()
+        visited = [False] * len(nums)
+        q.append((0,0))
+        visited[0] = True
+
+        while len(q):
+            count = len(q)
+
+            i , jump = q.popleft()
+            steps = nums[i]
+
+            if i == len(nums) - 1:
+                return jump
+            
+            for j in range(1, steps + 1):
+                if i + j < len(nums) and not visited[i + j]:
+                    visited[i + j] = True
+                    q.append((i + j, jump + 1))
+                
+
+
