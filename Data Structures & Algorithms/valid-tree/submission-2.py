db58440ab086266821752e48c36998f873159d88
shahaalap <68823276+shahaalap@users.noreply.github.com>
2026-04-27 14:42:13 -0400

    Add: valid-tree - submission-2

---
+++ Data Structures & Algorithms/valid-tree/submission-2.py
@@ -0,0 +1,44 @@
+class Solution:
+    def validTree(self, n: int, edges: List[List[int]]) -> bool:
+        edgelist = [[] for _ in range(n)]
+
+        for x,y in edges:
+            edgelist[x].append(y)
+            edgelist[y].append(x)
+
+        parents = {}
+        visited = [False] * n
+
+        def bfs(i):
+            q = deque()
+            q.append(i)
+            parents[i] = -1
+
+            while len(q):
+                node = q.popleft()
+                visited[node] = True
+
+                for nei in edgelist[node]:
+                    if not visited[nei]:
+                        q.append(nei)
+                        parents[nei] = node
+                    elif parents[node] != nei:
+                        return True
+
+            return False
+
+
+        islandcount = 0
+        for i in range(n):
+            if not visited[i]:
+                if islandcount == 1:
+                    return False
+
+                islandcount += 1
+                iscycle = bfs(i)
+                if iscycle:
+                    return False
+
+
+        return True
+
