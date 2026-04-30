544ccb04f1d2b928bcf3491fc287fb871ba28e9e
shahaalap <68823276+shahaalap@users.noreply.github.com>
2026-04-30 09:45:05 -0400

    Update: valid-parenthesis-string - submission-0

--- Data Structures & Algorithms/valid-parenthesis-string/submission-0.py
+++ Data Structures & Algorithms/valid-parenthesis-string/submission-0.py
@@ -1,23 +1,23 @@
 class Solution:
     def checkValidString(self, s: str) -> bool:
-        low , high = 0, 0
+        left, star = [], []
 
-        for c in s:
+        for i, c in enumerate(s):
             if c == '(':
-                low += 1
-                high += 1
-            elif c == ')':
-                low -= 1
-                high -= 1
+                left.append(i)
+            elif c == '*':
+                star.append(i)
             else:
-                low -= 1 #consider as )
-                high +=1 #consider as (
-            
-            if low < 0: #covers when * are considered to be closing character which can be ignored
-                low = 0
-            
-            if high < 0: #covers when we have a log of closing characters for which we dont have enough opening chracters despite of considering all * as opening
+                if len(left):
+                    left.pop()
+                elif not len(left) and len(star):
+                    star.pop()
+                else:
+                    return False
+
+        while len(left) and len(star):
+            if left.pop() > star.pop():
                 return False
 
-        return low == 0 # covers rest of the case like )( where you have pending low for which closing never was accounted for
-        
+        return len(left) == 0
+
